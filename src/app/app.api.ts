d9f964008a005a717defd430fa3770d0607dd990
unknown <henriquesombrio3@gmail.com>
2018-08-13 20:34:54 -0300

    Adicionado busca de dados

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,1 +1,1 @@
-export const MEAT_API = 'http://localhost:3000'
+export const MEAT_API = 'https://github.com/HenriqueSombrio/AppAngular-Sombrio/blob/master/db.json'
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://meatdelivery.s3-website-us-east-1.amazonaws.com'
 };
