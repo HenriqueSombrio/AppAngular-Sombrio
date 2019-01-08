97fe4f24913232e165891afdcc7ac5eba4d4d506
unknown <henriquesombrio3@gmail.com>
2019-01-08 20:11:13 -0200

    Adicionar pedido ao carrinho

--- src/app/restaurant-detail/shopping-cart/cart-item.model.ts
+++ src/app/restaurant-detail/shopping-cart/cart-item.model.ts
@@ -1,8 +1,8 @@
 import {MenuItem} from '../menu-item/menu-item.model'
 export class CartItem{
-    constructor(public MenuItem: MenuItem, public quantity: number = 1){}
+    constructor(public menuItem: MenuItem, public quantity: number = 1){}
     
     value(): number{
-        return this.MenuItem.price*this.quantity
+        return this.menuItem.price*this.quantity
     }
 }
--- src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.service.ts
@@ -9,7 +9,7 @@ export class ShoppingCartService{
     }
 
     addItem(item:MenuItem){
-        let foundItem = this.items.find((mItem)=> mItem.MenuItem.id === item.id)
+        let foundItem = this.items.find((mItem)=> mItem.menuItem.id === item.id)
         if(foundItem){
             foundItem.quantity = foundItem.quantity+1
         }else{
